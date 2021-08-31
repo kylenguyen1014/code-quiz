e471fd4d2424191ff6671361fd8fe3dd0e05ed1d
Kyle Nguyen <knguyen@kendausa.com>
2021-08-31 15:42:04 -0400

    make container responsive

--- code-quiz/src/App.tsx
+++ code-quiz/src/App.tsx
@@ -6,10 +6,25 @@ import { GridList } from './shared/constants/gridList';
 function App() {
   return (
     <div className="App">
-      <div className="App-grid-container">
-      {
-        GridList.map((grid, i) => <GridItem key={i} grid={grid}/>)
-      }
+      <div className='App-container'>
+
+        <h3 className="App-title">
+          <div/>
+          <span>
+            MEET THE TEAM
+          </span>
+          <div/>
+        </h3>
+        <div className="App-grid-container">
+        {
+          GridList.map((grid, i) => <GridItem key={i} grid={grid}/>)
+        }
+        </div>
+        <div className='App-footer'>
+          <div />
+          <span>♦</span>
+          <div />
+        </div>
       </div>
     </div>
   );
